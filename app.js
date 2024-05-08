25f41f53d8dbac8fc62799f5aba7845af2421514
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-05-08 11:47:16 +0545

    added multer for file upload / profile pic upload done

--- app.js
+++ app.js
@@ -6,14 +6,28 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const user = require("./models/user");
+const path = require("path");
+const crypto = require("crypto");
+const upload = require("./utils/multer");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/profile/upload", (req, res) => {
+  res.render("upload");
+});
+
+app.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
 });
 app.get("/profile", isLoggedIn, async (req, res) => {
   try {
@@ -54,10 +68,7 @@ app.post("/update/:id", isLoggedIn, async (req, res) => {
   res.redirect("/profile");
 });
 app.post("/delete/:id", isLoggedIn, async (req, res) => {
-  let post = await postModel.findOneAndDelete(
-  
-    { content: req.body.content }
-  );
+  let post = await postModel.findOneAndDelete({ content: req.body.content });
   res.redirect("/profile");
 });
 
