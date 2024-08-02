a6c24a8d82b459eb54dcfd106d3ec6d386ab4943
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-02 21:24:30 +0545

    memory Storage multer setup

--- utils/multer.js
+++ utils/multer.js
@@ -1,23 +1,7 @@
+// memeorystorage setup multer
+
 const multer = require("multer");
-const path = require("path");
-const crypto = require("crypto");
-
-
-// diskstorage
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12, function(err, name){
-        const fn = name.toString("hex") + path.extname(file.originalname);
-        cb(null, fn);
-    })
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 
 module.exports = upload;
